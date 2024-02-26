881eabecf4e86eb47fa0255a70f53e67c76213ea
kkenjeres <kkenjeres96@gmail.com>
2024-02-26 20:51:43 +0100

    shopify integration

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -2,34 +2,29 @@
 
 import React from "react";
 import Image from "next/image";
-import { products } from "../data/products";
 import { TiShoppingCart } from "react-icons/ti";
 import { useBasket } from "../context/BasketContext";
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
   const { addItem } = useBasket();
 
   return (
-    <div className="grid grid-cols-3 gap-4">
-      {products.map((product) => (
-        <div className="border border-black p-2" key={product.id}>
-          <Image
-            src={product.image}
-            width={900}
-            height={900}
-            alt={product.title}
-            objectFit="contain"
-            className="w-full h-[200px]"
-          />
-          <h2>{product.title}</h2>
-          <div className="flex text-[20px] items-center gap-2">
-            <button onClick={() => addItem({ ...product, quantity: 1 })}>
-              <TiShoppingCart className="bg-blue-600 text-white text-[40px] p-1" />
-            </button>
-            <span>$ {product.price}</span>
-          </div>
-        </div>
-      ))}
+    <div className="border border-black p-2">
+      <Image
+        src={product.image || '/placeholder-image.png'} // Добавьте плейсхолдер для изображения, если оно отсутствует
+        width={900}
+        height={900}
+        alt={product.title}
+        objectFit="contain"
+        className="w-full h-[200px]"
+      />
+      <h2>{product.title}</h2>
+      <div className="flex text-[20px] items-center gap-2">
+        <button onClick={() => addItem({ ...product, quantity: 1 })}>
+          <TiShoppingCart className="bg-blue-600 text-white text-[40px] p-1" />
+        </button>
+        <span>$ {product.price}</span>
+      </div>
     </div>
   );
 };
--- src/app/components/ProductList.tsx
+++ src/app/components/ProductList.tsx
@@ -1,9 +1,32 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
+
 const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProductsFromShopify = async () => {
+    const response = await fetch("/api/products", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    // Предполагаем, что структура данных соответствует ожиданиям
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProductsFromShopify();
+  }, []);
+
   return (
     <section className="w-[80%] m-auto">
-      <ProductCard />
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
     </section>
   );
 };
